50e0acf4acb042e68c5f13a3d6d162e46a79080a
rimjhim814 <rawalrimjhim@gmail.com>
2026-08-17 20:55:28 +0000

    Update conversion operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -29,10 +29,83 @@
 
 */
 
+/*
    let someNumber = 33
 
    let stringNumber = String(someNumber)
    console.log(stringNumber);
    console.log(typeof stringNumber);
+*/
+
+// *************** operations ****************
+
+
+/*
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**3); // power
+console.log(2/2);
+console.log(2%2);
+
+*/
+
+
+/*
+let str1 = "hello"
+let str2 = " rimjhim"
+
+let str3 = str1 + str2
+console.log(str3);
+*/
+
+
+/*
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2); // 122
+console.log(1 +2 + "2"); // 32
+*/
+
+
+/*
+ console.log(+true); //1 
+ console.log(+""); //0
+ // + - It tries to convert whatever comes after it into a number.
+*/
+
+/*
+ //prefix and postfix
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+//Expected output: "a:4, b:4"
+
+//x++ → use the value first, then increase it. "Give me x, THEN increase x."
+//++x → increase it first, then use the value. "Increase a, THEN give me a."
+
+ */
+ 
+
+
 
    
