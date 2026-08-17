b988818b63514bcd68774b98c766dd0046de9339
rimjhim814 <rawalrimjhim@gmail.com>
2026-08-17 20:14:20 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,38 @@
+ /*
+ let score = "33abc"
+
+
+ console.log(typeof (score));
+ console.log(typeof score);
+
+ let valueInNumber = Number(score)
+   console.log(typeof valueInNumber);
+   console.log(valueInNumber);//NaN - not a num
+
+   // "33" => 33
+   // "33abc" => NaN
+   // true => 1; false => 0
+   // null => 0
+   // undefined => NaN
+
+*/
+
+/*
+   let isLoggedIn = "rim"
+
+   let booleanIsLoggedIn = Boolean(isLoggedIn)
+   console.log(booleanIsLoggedIn);
+
+   // 1 => true; 0 => false
+   // "" => false
+   // "rimjhim" => true
+
+*/
+
+   let someNumber = 33
+
+   let stringNumber = String(someNumber)
+   console.log(stringNumber);
+   console.log(typeof stringNumber);
+
+   
